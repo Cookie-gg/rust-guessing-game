1cc5e3aa900791633996583c2fd45b87e5cdf1a0
cookie_gg <cookie.gg.dev@gmail.com>
2022-12-21 00:23:53 +0900

    add guessing game

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new(); // create new String instance
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess = match guess.trim().parse::<u32>() { // parse returns Result type (Ok, Err)
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        // println!("The secret number is: {secret_number}");
+
+        match guess.cmp(&secret_number) { // cmp returns Ordering type (Less, Greater, Equal)
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
